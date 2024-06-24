437d12d1b3f22d5dd9fa7e678275af3ecdaf9d90
Laszlo Pataki <lzlptk@gmail.com>
2024-06-24 11:48:32 +0200

    ops: deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  // This is for GitHub Pages
+  base: mode === 'production' ? '/dapp-penalty/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-});
+}));
